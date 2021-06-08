9a19596e04335715caf1574474f11f66171aec39
Иван Гаврилов <bzic1112@yandex.ru>
2021-06-08 14:01:17 +0300

    Добавьте файлы проекта.

---
+++ IValuesHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public interface IValuesHolder
+    {
+        List<string> Values { get; set; }
+        public void Add(string str);
+        public List<string> Get() => Values;
+    }
+}
---
+++ ValuesHolder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    class ValuesHolder : IValuesHolder
+    {
+        public ValuesHolder()
+        {
+            Values = new List<string>();
+        }
+
+        public List<string> Values { get; set; }
+        public List<string> Get() => Values;
+        public void Add(string str) { Values.Add(str); }
+    }
+}
